543b580e0a6f4949921ebc497b5cf89fe629f62a
Luís Mota <luisfernandogomesmota22@gmail.com>
2025-07-22 11:18:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
+print(f"Olá, {nome}!") 
+idade: str = input("Qual a tua idade? ")
+print(f"Idade, {idade}")
